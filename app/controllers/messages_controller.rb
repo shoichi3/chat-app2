9533ed4446df41315987b05a2ae086a7fcd5c2f2
shoichi3 <dece12shou@gmail.com>
2020-08-11 12:25:52 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,13 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
-    @message = @room.messages.new(message_params)
-    if @message.save
+    # @message = @room.messages.new(message_params) #気になる
+    if @room.messages.create(message_params)
       redirect_to room_messages_path(@room)
     else
       render :index
